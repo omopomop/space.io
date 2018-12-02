1e096bf16160702a0a14a18b3632caf727843333
Eric Wang <wang3021@purdue.edu>
2018-12-01 22:25:11 -0500

    acceleration and map added

--- app.js
+++ app.js
@@ -11,28 +11,55 @@
 
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
-
+var playerCount = 0;
 var Player = function(id){
 	var self = {
 		x:Math.floor(Math.random()*300)+10,
 		y:Math.floor(Math.random()*300)+10,
 		id:id,
-		number:""+Math.floor(10*Math.random()),
+		number:playerCount,
 		rb:false,
 		lb:false,
 		ub:false,
 		db:false,
-		maxSpd:10
+		maxSpd:10,
+		accelVert:.2,
+		accelHoriz:.2
 	}
 	self.updatePosition = function(){
-		if(self.rb)
-			self.x+=self.maxSpd;
-		if(self.lb)
-			self.x-=self.maxSpd;
-		if(self.ub)
-			self.y-=self.maxSpd;
-		if(self.db)
-			self.y+=self.maxSpd;
+		if(self.rb){
+			//console.log("RB IS BEING PRESSED");
+			self.x+=self.accelHoriz;
+			if(self.accelHoriz<self.maxSpd)
+				self.accelHoriz+=.2;
+		}
+			
+		if(self.lb){
+			self.x-=self.accelHoriz;
+			if(self.accelHoriz<self.maxSpd)
+				self.accelHoriz+=.2;
+		}
+		if(!self.lb && !self.rb){
+			self.accelHoriz = self.accelVert;
+		}
+		if(self.ub){
+			self.y-=self.accelVert;
+			if(self.accelVert<self.maxSpd)
+				self.accelVert+=.2;
+		}
+		if(self.db){
+			self.y+=self.accelVert;
+			if(self.accelVert<self.maxSpd)
+				self.accelVert+=.2;
+		}
+		if(!self.ub && !self.db){
+			self.accelVert = self.accelHoriz;
+		}
+		if(!self.ub && !self.db && !self.rb && !self.lb){
+			self.accelHoriz = .2;
+			self.accelVert = .2;
+		}
+		
 	}
 	
 	return self;
@@ -43,7 +70,7 @@ io.sockets.on('connection',function(socket){
 	socket.x = 0;
 	socket.y = 0;
 	SOCKET_LIST[socket.id] = socket;
-	
+	playerCount++;
 	var player = Player(socket.id);
 	PLAYER_LIST[socket.id] = player;
 	
@@ -52,6 +79,7 @@ io.sockets.on('connection',function(socket){
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
 		delete PLAYER_LIST[socket.id];
+		playerCount--;
 	});
 	
 	socket.on('keyPress',function(data){
